8d2a0cefcdfa11938243741343d34cb82e90a652
Ayushjoshi95 <aj9770894049@gmail.com>
2020-12-25 16:32:37 +0530

    online classes website

---
+++ about.php
@@ -0,0 +1,298 @@
+<?php
+require_once 'header.php';
+?>
+
+<section class="feature-area" style="margin-top: 30px;">
+<div class="container-fluid">
+<div class="feature-inner row">
+<div class="col-lg-2 col-md-6">
+<div class="feature-item d-flex">
+<i class="ti-book"></i>
+<div class="ml-20">
+<h4>New Classes</h4>
+<p>
+In the history of modern astronomy, there is probably no one greater leap forward.
+</p>
+</div>
+</div>
+</div>
+<div class="col-lg-2 col-md-6">
+<div class="feature-item d-flex">
+<i class="ti-cup"></i>
+<div class="ml-20">
+<h4>Top Courses</h4>
+<p>
+In the history of modern astronomy, there is probably no one greater leap forward.
+</p>
+</div>
+</div>
+</div>
+<div class="col-lg-2 col-md-6">
+<div class="feature-item d-flex border-right-0">
+<i class="ti-desktop"></i>
+<div class="ml-20">
+<h4>Full E-Books</h4>
+<p>
+In the history of modern astronomy, there is probably no one greater leap forward.
+</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+<!-- <section class="video-area section-gap-bottom">
+<div class="container">
+<div class="row align-items-center">
+<div class="col-lg-5">
+<div class="section-title text-white">
+<h2 class="text-white">
+Watch Our Trainers <br>
+in Live Action
+</h2>
+<p>
+In the history of modern astronomy, there is probably no one greater leap forward than the building and
+launch of the space telescope known as the Hubble.
+</p>
+</div>
+</div>
+<div class="offset-lg-1 col-md-6 video-left">
+<div class="owl-carousel video-carousel">
+<div class="single-video">
+<div class="video-part">
+<img class="img-fluid" src="img/video-img.jpg" alt="">
+<div class="overlay"></div>
+<a class="popup-youtube play-btn" href="https://www.youtube.com/watch?v=VufDd-QL1c0">
+<img class="play-icon" src="img/play-btn.png" alt="">
+</a>
+</div>
+<h4 class="text-white mb-20 mt-30">Learn Angular js Course for Legendary Persons</h4>
+<p class="text-white mb-20">
+In the history of modern astronomy, there is probably no one greater leap forward than the building and
+launch of the space telescope known as the Hubble.
+</p>
+</div>
+<div class="single-video">
+<div class="video-part">
+<img class="img-fluid" src="img/video-img.jpg" alt="">
+<div class="overlay"></div>
+<a class="popup-youtube play-btn" href="https://www.youtube.com/watch?v=VufDd-QL1c0">
+<img class="play-icon" src="img/play-btn.png" alt="">
+</a>
+</div>
+<h4 class="text-white mb-20 mt-30">Learn Angular js Course for Legendary Persons</h4>
+<p class="text-white mb-20">
+In the history of modern astronomy, there is probably no one greater leap forward than the building and
+launch of the space telescope known as the Hubble.
+</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+ -->
+
+ <section class="other-feature-area">
+<div class="container">
+<div class="feature-inner row">
+<div class="col-lg-12">
+<div class="section-title text-left">
+<h2>
+Features That <br />
+Can Avail By Everyone
+</h2>
+<p>
+If you are looking at blank cassettes on the web, you may be
+very confused at the difference in price. You may see some for
+as low as $.17 each.
+</p>
+</div>
+</div>
+<div class="col-lg-4 col-md-6">
+<div class="other-feature-item">
+<i class="ti-key"></i>
+<h4>Industrial Experinece</h4>
+<div>
+<p>
+Lorem ipsum dolor sit amet consec tetur adipisicing elit, sed
+do eiusmod tempor incididunt labore. Lorem ipsum dolor sit
+amet consec tetur adipisicing elit, sed do eiusmod tempor
+incididunt labore.
+</p>
+</div>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 mt--160">
+<div class="other-feature-item">
+<i class="ti-files"></i>
+<h4>Source File Included</h4>
+<div>
+<p>
+Lorem ipsum dolor sit amet consec tetur adipisicing elit, sed
+do eiusmod tempor incididunt labore. Lorem ipsum dolor sit
+amet consec tetur adipisicing elit, sed do eiusmod tempor
+incididunt labore.
+</p>
+</div>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 mt--260">
+<div class="other-feature-item">
+<i class="ti-medall-alt"></i>
+<h4>Internships</h4>
+<div>
+<p>
+Lorem ipsum dolor sit amet consec tetur adipisicing elit, sed
+do eiusmod tempor incididunt labore. Lorem ipsum dolor sit
+amet consec tetur adipisicing elit, sed do eiusmod tempor
+incididunt labore.
+</p>
+</div>
+</div>
+</div>
+<!-- <div class="col-lg-4 col-md-6">
+<div class="other-feature-item">
+<i class="ti-briefcase"></i>
+<h4></h4>
+<div>
+<p>
+Lorem ipsum dolor sit amet consec tetur adipisicing elit, sed
+do eiusmod tempor incididunt labore. Lorem ipsum dolor sit
+amet consec tetur adipisicing elit, sed do eiusmod tempor
+incididunt labore.
+</p>
+</div>
+</div>
+</div> -->
+<div class="col-lg-4 col-md-6 mt--100">
+<div class="other-feature-item">
+<i class="ti-crown"></i>
+<h4>Expert Mentors</h4>
+<div>
+<p>
+Lorem ipsum dolor sit amet consec tetur adipisicing elit, sed
+do eiusmod tempor incididunt labore. Lorem ipsum dolor sit
+amet consec tetur adipisicing elit, sed do eiusmod tempor
+incididunt labore.
+</p>
+</div>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 mt--160">
+<div class="other-feature-item">
+<i class="ti-headphone-alt"></i>
+<h4>Live Supports</h4>
+<div>
+<p>
+Lorem ipsum dolor sit amet consec tetur adipisicing elit, sed
+do eiusmod tempor incididunt labore. Lorem ipsum dolor sit
+amet consec tetur adipisicing elit, sed do eiusmod tempor
+incididunt labore.
+</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+
+<section class="testimonials-area section-gap">
+<div class="container">
+<div class="testi-slider owl-carousel" data-slider-id="1">
+<div class="row align-items-center">
+<div class="col-lg-5">
+<div class="item">
+<div class="testi-item">
+<img src="img/quote.png" alt="" />
+<div class="mt-40 text">
+<p>
+As conscious traveling Paup ers we must always be oncerned
+about our dear Mother Earth. If you think about it, you
+travel across her face and She is the host to your
+journey.
+</p>
+</div>
+<h4>Fanny Spencer</h4>
+<p>Chief Executive, Amazon</p>
+</div>
+</div>
+</div>
+<div class="offset-lg-1 col-lg-6">
+<img src="img/testimonial/t1.jpg" alt="" />
+</div>
+</div>
+<div class="row align-items-center">
+<div class="col-lg-5">
+  <div class="item">
+<div class="testi-item">
+<img src="img/quote.png" alt="" />
+<div class="mt-40 text">
+<p>
+As conscious traveling Paup ers we must always be oncerned
+about our dear Mother Earth. If you think about it, you
+travel across her face <br />
+and She is the host to your journey.
+</p>
+</div>
+<h4>Fanny Spencer</h4>
+<p>Chief Executive, Amazon</p>
+</div>
+</div>
+</div>
+<div class="offset-lg-1 col-lg-6">
+<img src="img/testimonial/t1.jpg" alt="" />
+</div>
+</div>
+</div>
+</div>
+</section>
+
+
+<section class="registration-area">
+<div class="container">
+<div class="row align-items-end">
+<div class="col-lg-5">
+<div class="section-title text-left text-white">
+<h2 class="text-white">
+Watch Our Trainers <br>
+in Live Action
+</h2>
+<p>
+If you are looking at blank cassettes on the web, you may be
+very confused at the difference in price. You may see some for
+as low as $.17 each.
+</p>
+</div>
+</div>
+<div class="offset-lg-3 col-lg-4 col-md-6">
+<div class="course-form-section">
+<h3 class="text-white">Register for Workshop</h3>
+<form class="course-form-area contact-page-form course-form text-right" id="myForm" action="https://preview.colorlib.com/theme/eclipse/mail.html" method="post" style="margin-top: 25px;">
+<div class="form-group col-md-12">
+<input type="text" class="form-control" id="name" name="name" placeholder="Name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Name'">
+</div>
+<div class="form-group col-md-12">
+<input type="text" class="form-control" id="subject" name="subject" placeholder="Phone Number" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Phone Number'">
+</div>
+<div class="form-group col-md-12">
+<input type="email" class="form-control" id="email" name="email" placeholder="Email Address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email Address'">
+</div>
+<div class="col-lg-12 text-center">
+<button class="btn text-uppercase">Submit</button>
+</div>
+</form>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+</body>
+<?php
+  require_once "js-links.php";
+  ?>
+<!-- Mirrored from preview.colorlib.com/theme/eclipse/ by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 25 Dec 2020 05:10:34 GMT -->
+</html>
---
+++ header.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
+
+<!-- Mirrored from preview.colorlib.com/theme/eclipse/ by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 25 Dec 2020 05:10:20 GMT -->
+<head>
+
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+<link rel="shortcut icon" href="img/fav.png" />
+
+<meta name="author" content="colorlib" />
+
+<meta name="description" content="" />
+
+<meta name="keywords" content="" />
+
+<meta charset="UTF-8" />
+
+<title>Cyber Flow</title>
+<link href="https://fonts.googleapis.com/css?family=Playfair+Display:900|Roboto:400,400i,500,700" rel="stylesheet" />
+
+<link rel="stylesheet" href="css/linearicons.css" />
+<link rel="stylesheet" href="css/font-awesome.min.css" />
+<link rel="stylesheet" href="css/bootstrap.css" />
+<link rel="stylesheet" href="css/magnific-popup.css" />
+<link rel="stylesheet" href="css/owl.carousel.css" />
+<link rel="stylesheet" href="css/nice-select.css">
+<link rel="stylesheet" href="css/hexagons.min.css" />
+<link rel="stylesheet" href="../../../cdn.jsdelivr.net/themify-icons/0.1.2/css/themify-icons.css" />
+<link rel="stylesheet" href="css/main.css" />
+</head>
+<body>
+
+<header class="default-header">
+<nav class="navbar navbar-expand-lg  navbar-light">
+<div class="container">
+<a class="navbar-brand" href="index-2.html">
+<p style="color:white;">Cyber Flow</p>
+</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+<span class="lnr lnr-menu"></span>
+</button>
+<div class="collapse navbar-collapse justify-content-end align-items-center" id="navbarSupportedContent">
+<ul class="navbar-nav">
+<li><a href="index.php">Home</a></li>
+<li><a href="about.php">About</a></li>
+<!-- <li><a href="courses.html">Courses</a></li> -->
+
+<!-- <li><a href="contacts.html">Contacts</a></li>
+<li>
+<button class="search">
+<span class="lnr lnr-magnifier" id="search"></span>
+</button>
+</li> -->
+</ul>
+</div>
+</div>
+</nav>
+<!-- <div class="search-input" id="search-input-box">
+<div class="container">
+<form class="d-flex justify-content-between">
+<input type="text" class="form-control" id="search-input" placeholder="Search Here" />
+<button type="submit" class="btn"></button>
+<span class="lnr lnr-cross" id="close-search" title="Close Search"></span>
+</form>
+</div>
+</div> -->
+</header>
